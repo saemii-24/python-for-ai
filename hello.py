7e291a090950cef86c43cfe8facac6591c928ec9
saemii-24 <hims2419@gmail.com>
2026-01-13 08:35:28 +0900

    feat: class

--- hello.py
+++ hello.py
@@ -1,12 +1,46 @@
-import requests
-from IPython.display import JSON
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
 
-def get_specific_weather(latitude, longitude):
-    response = requests.get(
-        f"https://api.open-meteo.com/v1/forecast"
-        f"?latitude={latitude}&longitude={longitude}&current_weather=true"
-    )
-    r = response.json()
-    return r["current_weather"]['temperature']
+    def bark(self):
+        return "Woof!"
 
-print(get_specific_weather(37.5665, 126.9780)) # 서울의 위도와 경도
+    def __str__(self):
+        return f"Dog named {self.name}"
+
+    def __repr__(self):
+        return f"Dog(name='{self.name}', breed='{self.breed}')"
+
+
+class Cat:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+jerry = Dog("Jerry", "Beagle")
+
+print(jerry)
+print(jerry.bark())
+
+
+class Animal:
+    def __init__(self, species, age):
+        self.species = species
+        self.age = age
+    
+    def info(self):
+        return f"{self.species}, Age: {self.age}"
+
+class Bird(Animal):
+    def __init__(self, species, age, can_fly):
+        super().__init__(species, age)
+        self.can_fly = can_fly
+    
+    # def info(self):
+    #     fly_status = "can fly" if self.can_fly else "cannot fly"
+    #     return f"{super().info()}, {fly_status}"
+
+parrot = Bird("Parrot", 2, True)
+print(parrot.info())
