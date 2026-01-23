6188c93591615df80c2d751f2fdf0f41c7549dd3
saemii-24 <hims2419@gmail.com>
2026-01-24 06:12:35 +0900

    feat: 형태소 분석 시작

--- pandas_test.py
+++ pandas_test.py
@@ -1,22 +1,39 @@
 import pandas as pd
 import re
+from konlpy.tag import Okt
 
 data = {
-    "text": [
-        "This app is amazing and very useful",
-        "Terrible update, 너무 불편해요",
-        "I like the design but it crashes often",
-        "완전 별로임 다시는 안씀",
+    "review": [
+        "앱이 너무 느리고 자꾸 튕겨요",
+        "업데이트 이후로 훨씬 좋아졌어요 ㅎ",
+        "디자인은 예쁜데 사용하기 불편함 -_-",
+        "완전 최악입니다 다시는 안 씀 ^^;;;;",
+        "속도도 빠르고 기능도 만족스러워요",
     ]
 }
 
 df = pd.DataFrame(data)
+print(df)
+
+# 특무누자 제거, 불필요한 공백 제거
 
 
 def clean_text(text):
-    text = text.lower()  # 소문자
-    text = re.sub(r"[^a-zA-Z가-힣\s]", "", text)  # 특수문자 제거
+    text = re.sub(r"[^가-힣\s]", "", text)
+    text = re.sub(r"\s+", " ", text).strip()
     return text
 
 
-df["clean_text"] = df["text"].apply(clean_text)
+df["cleaned_review"] = df["review"].apply(clean_text)
+print(df)
+
+# 형태소 분석 및 명사 추출
+okt = Okt()
+
+
+def tokenize(text):
+    return [word for word, post in okt.pos(text) if post in ["Noun", "Adjective"]]
+
+
+df["tokens"] = df["cleaned_review"].apply(tokenize)
+print(df(["reviews", "tokens"]))
